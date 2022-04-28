389d8e5b546427449bb3ff47987ca963200fa1af
Nikki van Braam <55128335+NikkivB@users.noreply.github.com>
2022-04-28 15:06:37 +0200

    day 9

---
+++ scripts/day9.py
@@ -0,0 +1,44 @@
+import json
+
+
+def get_input():
+    file = open("../inputs/day9.txt", "r")
+    data = file.read().split("\n")
+    file.close()
+    return data
+
+
+def is_valid(input, index, value, preamble):
+    for num in range(index-preamble, index):
+        for num2 in range(index-preamble, index):
+            if int(input[num]) + int(input[num2]) == int(value):
+                return True
+                break
+    return False
+
+
+def part1(input, preamble):
+    for i in range(preamble, len(input)):
+        if not is_valid(input, i, int(input[i]), preamble):
+            return input[i]
+
+
+def part2(input, value):
+    for a in range(len(input)):
+        values = []
+        values.append(int(input[a]))
+        for b in range(a+1, len(input)):
+            values.append(int(input[b]))
+            if sum(values) == int(value):
+                return values
+            if sum(values) >= int(value):
+                break
+
+
+
+gotten_input = get_input()
+part1_answer = part1(gotten_input, 25)
+part2_answer = part2(gotten_input, part1_answer)
+
+print('part 1', part1_answer)
+print('part 2', max(part2_answer) + min(part2_answer))
